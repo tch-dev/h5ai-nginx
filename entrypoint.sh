9b4653459b55fc6d3bd2c381711ee39b2bb3deed
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-02-07 20:35:34 +0100

    Updating config to have sol support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Generate password protected area
-htpasswd -cb /etc/nginx/.htpasswd ${USERNAME} ${PASSWORD}
+#htpasswd -cb /etc/nginx/.htpasswd ${USERNAME} ${PASSWORD}
 
 # Start php fpm
-/etc/init.d/php7.0-fpm start
+/etc/init.d/php7.3-fpm start
 
 # Start nginx
 nginx -g "daemon off;"
