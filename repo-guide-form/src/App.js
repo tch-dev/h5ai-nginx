7ae926ae401e71dbbcaaad522ec35149ba21dafc
kuzdogan <kaanuzdogan@hotmail.com>
2021-10-13 17:30:59 +0200

    Add form to navigate the user to the contract

--- repo-guide-form/src/App.js
+++ repo-guide-form/src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import "./App.css";
+import chains from "./chains";
 
 function App() {
+  const [selectedMatch, setSelectedMatch] = useState("full_match");
+  const [address, setAddress] = useState();
+  const [chainId, setChainId] = useState(1);
+  const [validated, setValidated] = useState(false);
+
+  const matches = [
+    { name: "Full Match", value: "full_match" },
+    { name: "Partial Match", value: "partial_match" },
+  ];
+
+  const generateRepoURI = (address, chainId, match) => {
+    return `/contracts/${match}/${chainId}/${address}`;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      setValidated(true);
+      return;
+    }
+
+    const uri = generateRepoURI(address, chainId, selectedMatch);
+    window.location.href = uri;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className="card">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <div className="mb-3">
+            {" "}
+            Enter the contract details you want to see{" "}
+          </div>
+          <Form.Group className="mb-3" controlId="form-address">
+            <Form.Label className="label">Contract Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="0x2fa5b..."
+              required
+              pattern="0x[a-fA-F0-9]{40}$"
+              onChange={(e) => setAddress(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(e);
+                }
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid address
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="form-chain">
+            <Form.Label className="label">Chain</Form.Label>
+            <Form.Select
+              aria-label="select chain"
+              onChange={(e) => setChainId(e.target.value)}
+            >
+              {chains.map((chainObj, i) => {
+                return (
+                  <option key={`chain-option-${i}`} value={chainObj.id}>
+                    {" "}
+                    {chainObj.label}{" "}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3 match-select-container">
+            {matches.map((match, idx) => (
+              <Button
+                key={idx}
+                id={`match-${idx}`}
+                type="radio"
+                variant={
+                  selectedMatch === match.value ? "primary" : "outline-primary"
+                }
+                name="matches"
+                value={match.value}
+                checked={selectedMatch === match.value}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedMatch(e.currentTarget.value);
+                }}
+              >
+                {match.name}
+              </Button>
+            ))}
+          </Form.Group>
+          <Button
+            variant="success"
+            type="submit"
+            size="lg"
+            style={{ width: "100%" }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </Card>
     </div>
   );
 }
---
+++ repo-guide-form/src/chains.js
@@ -0,0 +1,42 @@
+const chains = [
+  { value: "mainnet", label: "Ethereum Mainnet", id: 1 },
+  { value: "ropsten", label: "Ropsten", id: 3 },
+  { value: "rinkeby", label: "Rinkeby", id: 4 },
+  { value: "kovan", label: "Kovan", id: 42 },
+  { value: "goerli", label: "Görli", id: 5 },
+  { value: "xdai", label: "xDai", id: 100 },
+  { value: "sokol", label: "Sokol", id: 77 },
+  {
+    value: "binance smart chain mainnet",
+    label: "Binance Smart Chain Mainnet",
+    id: 56,
+  },
+  {
+    value: "binance smart chain testnet",
+    label: "Binance Smart Chain Testnet",
+    id: 97,
+  },
+  { value: "matic mainnet", label: "Polygon (previously Matic)", id: 137 },
+  {
+    value: "mumbai testnet",
+    label: "Mumbai Testnet (Polygon/Matic)",
+    id: 80001,
+  },
+  { value: "celo mainnet", label: "Celo Mainnet", id: 42220 },
+  { value: "alfajores testnet", label: "Celo Alfajores Testnet", id: 44787 },
+  { value: "baklava testnet", label: "Celo Baklava Testnet", id: 62320 },
+  { value: "avalanche testnet", label: "Avalanche Fuji Testnet", id: 43113 },
+  { value: "avalanche mainnet", label: "Avalanche Mainnet", id: 43114 },
+  { value: "arbitrum one", label: "Arbitrum Mainnet", id: 42161 },
+  { value: "arbitrum rinkeby", label: "Arbitrum Testnet Rinkeby", id: 421611 },
+  { value: "telos mainnet", label: "Telos EVM Mainnet", id: 40 },
+  { value: "telos testnet", label: "Telos EVM Testnet", id: 41 },
+  { value: "ubiq", label: "Ubiq", id: 8 },
+  {
+    value: "oneledger testnet",
+    label: "OneLedger Testnet Frankenstein",
+    id: 4216137055,
+  },
+];
+
+export default chains;
